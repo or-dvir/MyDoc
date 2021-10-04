64e2a434ff511b8c862536fb8caa2e207bf8a1d4
Or Dvir <or_dvir@hotmail.com>
2021-10-04 15:52:26 +0200

    added empty view to MyDoctors screen

--- app/src/main/java/com/hotmail/or_dvir/mydoc/ui/my_doctors/MyDoctorsScreen.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/my_doctors/MyDoctorsScreen.kt
@@ -1,11 +1,14 @@
 package com.hotmail.or_dvir.mydoc.ui.my_doctors
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -13,9 +16,12 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.hotmail.or_dvir.mydoc.R
 import com.hotmail.or_dvir.mydoc.ui.my_doctors.MyDoctorsViewModel.MyDoctorsUiState
@@ -60,6 +66,11 @@ fun ScreenContent(viewModel: MyDoctorsViewModel)
     ) {
         val uiState by viewModel.uiState.observeAsState(MyDoctorsUiState())
 
+        if (uiState.doctors.isEmpty())
+        {
+            EmptyView()
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -74,6 +85,33 @@ fun ScreenContent(viewModel: MyDoctorsViewModel)
         if (uiState.isLoading)
         {
             LoadingIndicatorFullScreen()
+        }
+    }
+}
+
+@Composable
+fun EmptyView()
+{
+    //todo make this look nicer.
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                modifier = Modifier.size(150.dp).border(2.dp, Color.Red),
+                painter = painterResource(id = R.drawable.ic_group_filled),
+                contentDescription = ""
+            )
+
+            Text(
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h5,
+                text = stringResource(id = R.string.emptyView_myDoctors)
+            )
         }
     }
 }
