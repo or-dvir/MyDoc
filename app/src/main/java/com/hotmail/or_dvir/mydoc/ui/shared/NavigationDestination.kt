3a9b4bb86f599b58655aa168fac7f52e296ae691
Or Dvir <or_dvir@hotmail.com>
2021-10-05 21:52:34 +0200

    Update NavigationDestination.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/NavigationDestination.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/NavigationDestination.kt
@@ -4,6 +4,7 @@ import java.util.UUID
 
 sealed class NavigationDestination
 {
+    object popStack : NavigationDestination()
     object NewDoctorScreen : NavigationDestination()
     class DoctorDetailsScreen(val doctorId: UUID) : NavigationDestination()
 }
