03f6c316bc8f1f5dff1d1d9f6f2b8b10bf42196c
Or Dvir <or_dvir@hotmail.com>
2021-10-05 21:22:32 +0200

    added edit and delete action to doctor details (not finished)

--- app/src/main/java/com/hotmail/or_dvir/mydoc/ui/doctor_details/DoctorDetailsScreen.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/doctor_details/DoctorDetailsScreen.kt
@@ -6,16 +6,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -31,7 +38,7 @@ fun DoctorsDetailsScreen(viewModel: DoctorDetailsViewModel, onBackButtonClicked:
     // look into landscape mode
     // add option to delete doctor. here or from list of doctors?
     //      if here, it takes an extra step so its safer
-    // add back button on action bar
+    // add option to edit doctor
 
     MyDocTheme {
         val uiState by viewModel.uiState.observeAsState(DoctorDetailsUiState())
@@ -39,6 +46,7 @@ fun DoctorsDetailsScreen(viewModel: DoctorDetailsViewModel, onBackButtonClicked:
 
         Scaffold(
             scaffoldState = scaffoldState,
+            content = { ScreenContent(viewModel, uiState) },
             topBar = {
                 TopAppBar(
                     title = { Text(uiState.doctor.name) },
@@ -49,10 +57,75 @@ fun DoctorsDetailsScreen(viewModel: DoctorDetailsViewModel, onBackButtonClicked:
                                 stringResource(id = R.string.contentDescription_back)
                             )
                         }
+                    },
+                    actions = {
+                        TopBarActions(
+                            onDelete = { viewModel.deleteDoctor() }
+                        )
                     }
                 )
-            },
-            content = { ScreenContent(viewModel, uiState) },
+            }
+        )
+    }
+}
+
+@Composable
+fun DeleteConfirmationDialog(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+)
+{
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        text = {
+            Text(
+                text = stringResource(id = R.string.deleteDoctorConfirmation),
+                style = MaterialTheme.typography.subtitle1,
+            )
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirm() }) {
+                Text(stringResource(id = R.string.yes))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismiss() }) {
+                Text(stringResource(id = R.string.no))
+            }
+        }
+    )
+}
+
+@Composable
+fun TopBarActions(onDelete: () -> Unit)
+{
+    var showDeleteConfirmation by remember { mutableStateOf(false) }
+
+    if (showDeleteConfirmation)
+    {
+        DeleteConfirmationDialog(
+            onDismiss = { showDeleteConfirmation = false },
+            onConfirm = { onDelete() }
+        )
+    }
+
+    IconButton(onClick = {
+        showDeleteConfirmation = true
+    }) {
+        Icon(
+            tint = Color.White,
+            painter = painterResource(id = R.drawable.ic_delete),
+            contentDescription = stringResource(id = R.string.contentDescription_delete)
+        )
+    }
+
+    IconButton(onClick = { /* todo */ }) {
+        Icon(
+            tint = Color.White,
+            painter = painterResource(id = R.drawable.ic_edit),
+            contentDescription = stringResource(id = R.string.contentDescription_edit)
         )
     }
 }
