d6d653bc853f93b599a0b6ddd89ab42cfdcf3906
Or Dvir <or_dvir@hotmail.com>
2021-10-05 18:13:23 +0200

    Create BaseViewModel.kt

---
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/BaseViewModel.kt
@@ -0,0 +1,38 @@
+package com.hotmail.or_dvir.mydoc.ui.shared
+
+import android.app.Application
+import androidx.annotation.NonNull
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+
+abstract class BaseViewModel<UiState>(app: Application) : AndroidViewModel(app), KoinComponent
+{
+    internal val mainDispatcher = Dispatchers.Main
+
+    private val _uiState = MutableLiveData(this.initUiState())
+    val uiState: LiveData<UiState> = _uiState
+
+    private val navDestination = Channel<NavigationDestination>()
+    val navDestinationFlow = navDestination.receiveAsFlow()
+
+    abstract fun initUiState(): UiState
+
+    fun navigate(event: NavigationDestination)
+    {
+        viewModelScope.launch(mainDispatcher) {
+            navDestination.send(event)
+        }
+    }
+
+    internal fun updateUiState(@NonNull newState: UiState)
+    {
+        _uiState.value = newState!!
+    }
+}
