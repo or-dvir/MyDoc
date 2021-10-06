76082de06467ddac0515342e8e7674f797045a2a
Or Dvir <or_dvir@hotmail.com>
2021-10-06 19:32:27 +0200

    Update BaseFragment.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/BaseFragment.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/BaseFragment.kt
@@ -1,5 +1,7 @@
 package com.hotmail.or_dvir.mydoc.ui.shared
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -85,5 +87,19 @@ abstract class BaseFragment<out VM : BaseViewModel<out Any>> : Fragment()
                 }
             }
         }
+    }
+
+    internal fun openMaps(query: String)
+    {
+        //todo do i need this?
+        //      mapIntent.setPackage("com.google.android.apps.maps")
+        // see https://developers.google.com/maps/documentation/urls/android-intents#intent_requests
+
+        startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/maps/search/?api=1&query=$query")
+            )
+        )
     }
 }
