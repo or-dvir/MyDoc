4a995001eb6e503b667029b793f1ea5dd175feeb
Or Dvir <or_dvir@hotmail.com>
2021-10-06 19:30:41 +0200

    Update Address.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/models/Address.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/models/Address.kt
@@ -1,11 +1,26 @@
 package com.hotmail.or_dvir.mydoc.models
 
 data class Address(
-    val street: String,
-    val houseNumber: String,
-    val apartmentNumber: String,
-    val floor: Int,
-    val postCode: Int,
-    val city: String,
-    val country: String
+    val street: String = "",
+    val houseNumber: String = "",
+    val apartmentNumber: String = "",
+    val floor: Int = DEFAULT_INT,
+    val postCode: Int = DEFAULT_INT,
+    val city: String = "",
+    val country: String = ""
 )
+{
+    private companion object
+    {
+        private const val DEFAULT_INT = -10000
+    }
+
+    fun formatAddress(): String {
+        val builder = StringBuilder()
+
+        finish me.
+                handle case where no information is available
+
+        return builder.toString()
+    }
+}
