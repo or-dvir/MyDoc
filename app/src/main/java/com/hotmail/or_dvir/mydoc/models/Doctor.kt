fefc929e2d2216ca215a51e5d7f812021cfddb0e
Or Dvir <or_dvir@hotmail.com>
2021-10-06 19:31:18 +0200

    Update Doctor.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/models/Doctor.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/models/Doctor.kt
@@ -5,9 +5,14 @@ import java.util.UUID
 data class Doctor(
     val id: UUID,
     val name: String,
-    val specialty: String
+    val specialty: String = "",
+    val address: Address = Address()
 )
 
+add address to
+    list of doctors (quick navigation!)
+    doctor details (add navigate button)
+    new/edit doctor
+
 //todo
-// address
 // opening times
