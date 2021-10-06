38f8b1585f61fe1adf6a4d0374acca53548fdbb4
Or Dvir <or_dvir@hotmail.com>
2021-10-06 19:31:26 +0200

    Update DoctorsRepositoryImpl.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/repositories/DoctorsRepositoryImpl.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/repositories/DoctorsRepositoryImpl.kt
@@ -1,21 +1,31 @@
 package com.hotmail.or_dvir.mydoc.repositories
 
+import com.hotmail.or_dvir.mydoc.models.Address
 import com.hotmail.or_dvir.mydoc.models.Doctor
 import java.util.UUID
 
 class DoctorsRepositoryImpl : DoctorsRepository
 {
+    private val dummyAddress = Address("bla", "4")
+
+    private val dummyDoctor = Doctor(
+        UUID.randomUUID(),
+        "Dr Evil",
+        "evilness",
+        dummyAddress
+    )
+
     override suspend fun getDoctor(id: UUID): Doctor
     {
         //TODO("not implemented")
-        return Doctor(UUID.randomUUID(), "Dr Evil", "evilness")
+        return dummyDoctor
     }
 
     override suspend fun getAll(): List<Doctor>
     {
         //TODO("not implemented")
         return List(50) { index ->
-            Doctor(UUID.randomUUID(), "Dr. $index", "special $index")
+            Doctor(UUID.randomUUID(), "Dr. $index", "special $index", dummyAddress)
         }
     }
 
