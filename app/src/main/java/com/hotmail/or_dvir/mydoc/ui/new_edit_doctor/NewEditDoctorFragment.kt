b5d50c93af760aa362a5e821805973c45d85876d
Or Dvir <or_dvir@hotmail.com>
2021-10-06 12:42:26 +0200

    Update NewEditDoctorFragment.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/ui/new_edit_doctor/NewEditDoctorFragment.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/new_edit_doctor/NewEditDoctorFragment.kt
@@ -1,16 +1,26 @@
 package com.hotmail.or_dvir.mydoc.ui.new_edit_doctor
 
+import android.os.Bundle
+import android.view.View
 import androidx.compose.runtime.Composable
+import androidx.navigation.fragment.navArgs
 import com.hotmail.or_dvir.mydoc.R
 import com.hotmail.or_dvir.mydoc.ui.shared.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.UUID
 
 class NewEditDoctorFragment : BaseFragment<NewEditDoctorViewModel>()
 {
+    private val fragArgs: NewEditDoctorFragmentArgs by navArgs()
     override val viewModel: NewEditDoctorViewModel by viewModel()
-
     override val fragmentId = R.id.newEditDoctorFragment
 
     @Composable
     override fun ScreenContent() = NewEditDoctorScreen(viewModel)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.loadDoctor(UUID.fromString(fragArgs.doctorId))
+    }
 }
