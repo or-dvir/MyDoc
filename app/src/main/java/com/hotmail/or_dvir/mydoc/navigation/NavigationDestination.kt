a56c979f9c83c9e91e1eed227839f04945540238
Or Dvir <or_dvir@hotmail.com>
2021-11-04 17:07:02 +0100

    Update NavigationDestination.kt

--- app/src/main/java/com/hotmail/or_dvir/mydoc/navigation/NavigationDestination.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/navigation/NavigationDestination.kt
@@ -5,6 +5,10 @@ import java.util.UUID
 sealed class NavigationDestination
 {
     object PopStack : NavigationDestination()
+
+    //todo do i need the doctors list screen? its the "home screen".
+    //  will other screens need to navigate directly to it?
+    //  if so, watch out for ciruclar navigation!!!
     class NewEditDoctorScreen(val doctorId: UUID?) : NavigationDestination()
     class DoctorDetailsScreen(val doctorId: UUID) : NavigationDestination()
 }
