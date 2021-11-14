364ad557a021d93d329a009038d07a411d00ac32
Or Dvir <or_dvir@hotmail.com>
2021-11-14 21:38:04 +0100

    added ExposedDropDownMenu from stack overflow

--- app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/Compsables.kt
+++ app/src/main/java/com/hotmail/or_dvir/mydoc/ui/shared/Compsables.kt
@@ -2,30 +2,36 @@ package com.hotmail.or_dvir.mydoc.ui.shared
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,11 +40,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -52,139 +63,140 @@ import androidx.compose.ui.unit.toSize
 import com.hotmail.or_dvir.mydoc.R
 import com.hotmail.or_dvir.mydoc.ui.theme.Typography
 
+//region copied from https://stackoverflow.com/questions/67111020/exposed-drop-down-menu-for-jetpack-compose#answer-69042850
 @Composable
 fun ExposedDropDownMenu(
-    label: String,
-    menuItems: List<String>,
-    onMenuItemSelected: (index: Int, item: String) -> Unit,
-    modifier: Modifier = Modifier,
-    leadingIcon: @Composable (() -> Unit)? = null,
+    values: List<String>,
+    onChange: (Int, String) -> Unit,
+    label: @Composable () -> Unit,
+    modifier: Modifier,
+    backgroundColor: Color = MaterialTheme.colors.onSurface.copy(alpha = TextFieldDefaults.BackgroundOpacity),
+    shape: Shape = MaterialTheme.shapes.small.copy(
+        bottomEnd = ZeroCornerSize,
+        bottomStart = ZeroCornerSize
+    )
 )
 {
-    ExposedDropdownMenu will be added in compose 1.1!!!
-    you can try the preview with this version: 1.1.0-beta02
-    there is a problem... the default click behaviour of text field does not allow
-    to open the menu.
-            try this:
-                https://stackoverflow.com/questions/67111020/exposed-drop-down-menu-for-jetpack-compose#answer-69042850
-    
-    //todo
-    // initially selected item
-    // option for nothing selected
+    ExposedDropDownMenuImpl(
+        values = values,
+        onChange = onChange,
+        label = label,
+        modifier = modifier,
+        backgroundColor = backgroundColor,
+        shape = shape,
+        decorator = { color, width, content ->
+            Box(
+                Modifier
+                    .drawBehind {
+                        val strokeWidth = width.value * density
+                        val y = size.height - strokeWidth / 2
+                        drawLine(
+                            color,
+                            Offset(0f, y),
+                            Offset(size.width, y),
+                            strokeWidth
+                        )
+                    }
+            ) {
+                content()
+            }
+        }
+    )
+}
 
-    //todo update value when clicking menu item
-    var selectedItem by remember { mutableStateOf("") }
-    var isMenuExpanded by remember { mutableStateOf(false) }
+//copied from stack overflow (link in region name)
+@Composable
+private fun ExposedDropDownMenuImpl(
+    values: List<String>,
+    onChange: (Int, String) -> Unit,
+    label: @Composable () -> Unit,
+    modifier: Modifier,
+    backgroundColor: Color,
+    shape: Shape,
+    decorator: @Composable (Color, Dp, @Composable () -> Unit) -> Unit
+)
+{
+    var selectedIndex by remember { mutableStateOf(0) }
+    var expanded by remember { mutableStateOf(false) }
     var textFieldSize by remember { mutableStateOf(Size.Zero) }
 
-    //assume menu is collapsed by default
-    var trailingIconImage: ImageVector = Icons.Default.KeyboardArrowDown
-    var trailingIconContentDescription: String =
-        stringResource(R.string.contentDescription_openMenu)
+    val indicatorColor =
+        if (expanded) MaterialTheme.colors.primary.copy(alpha = ContentAlpha.high)
+        else MaterialTheme.colors.onSurface.copy(alpha = TextFieldDefaults.UnfocusedIndicatorLineOpacity)
 
-    if (isMenuExpanded)
-    {
-        trailingIconImage = Icons.Default.KeyboardArrowUp
-        trailingIconContentDescription =
-            stringResource(R.string.contentDescription_closeMenu)
-    }
+    val indicatorWidth = (if (expanded) 2 else 1).dp
 
-    //todo do i need this column???
-    Column(modifier) {
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned {
-                    //This value is used to assign to the DropDown the same width
-                    textFieldSize = it.size.toSize()
+    val labelColor =
+        if (expanded) MaterialTheme.colors.primary.copy(alpha = ContentAlpha.high)
+        else MaterialTheme.colors.onSurface.copy(ContentAlpha.medium)
+
+    val trailingIconColor =
+        MaterialTheme.colors.onSurface.copy(alpha = TextFieldDefaults.IconOpacity)
+    val rotation: Float by animateFloatAsState(if (expanded) 180f else 0f)
+    val focusManager = LocalFocusManager.current
+
+    Column(modifier = modifier.width(IntrinsicSize.Min)) {
+        decorator(indicatorColor, indicatorWidth) {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .background(color = backgroundColor, shape = shape)
+                    .onGloballyPositioned { textFieldSize = it.size.toSize() }
+                    .clip(shape)
+                    .clickable {
+                        expanded = !expanded
+                        focusManager.clearFocus()
+                    }
+                    .padding(start = 16.dp, end = 12.dp, top = 7.dp, bottom = 10.dp)
+            ) {
+                Column(Modifier.padding(end = 32.dp)) {
+                    ProvideTextStyle(value = MaterialTheme.typography.caption.copy(color = labelColor)) {
+                        label()
+                    }
+                    Text(
+                        text = values[selectedIndex],
+                        modifier = Modifier.padding(top = 1.dp)
+                    )
                 }
-                .clickable { isMenuExpanded = !isMenuExpanded },
-            readOnly = true,
-            label = { Text(label) },
-            value = selectedItem,
-            onValueChange = {
-                //todo check if this is called on recomposition (i.e. orientation change)
-                onMenuItemSelected(menuItems.indexOf(it), it)
-                isMenuExpanded = false
-            },
-            leadingIcon = leadingIcon,
-            trailingIcon = {
-                //todo do i need to make this clickable???
                 Icon(
-                    imageVector = trailingIconImage,
-                    contentDescription = trailingIconContentDescription,
-                    modifier = modifier.clickable { isMenuExpanded = !isMenuExpanded }
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = "Change",
+                    tint = trailingIconColor,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(top = 4.dp)
+                        .rotate(rotation)
                 )
-            }
-        )
 
-        val menuWidth = with(LocalDensity.current) {
-            textFieldSize.width.toDp()
+            }
         }
 
         DropdownMenu(
-            modifier = Modifier.width(menuWidth),
-            expanded = isMenuExpanded,
-            onDismissRequest = { isMenuExpanded = false }
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textFieldSize.width.toDp() })
         ) {
-            menuItems.forEach {
+            values.forEachIndexed { i, v ->
+//                val scope = rememberCoroutineScope()
                 DropdownMenuItem(
-                    onClick = { selectedItem = it }
+                    onClick = {
+                        onChange(i, v)
+                        selectedIndex = i
+                        expanded = false
+//                        scope.launch {
+//                            delay(150)
+//                            expanded = false
+//                        }
+                    }
                 ) {
-                    Text(it)
+                    Text(v)
                 }
             }
         }
     }
 }
-
-//@Composable
-//fun FunctionToCopy()
-//{
-//    var expanded by remember { mutableStateOf(false) }
-//    val suggestions = listOf("Item1", "Item2", "Item3")
-//    var selectedText by remember { mutableStateOf("") }
-//
-//    var textfieldSize by remember { mutableStateOf(Size.Zero) }
-//
-//    val icon = if (expanded)
-//        Icons.Filled.ArrowDropUp //it requires androidx.compose.material:material-icons-extended
-//    else
-//        Icons.Filled.ArrowDropDown
-//
-//
-//    Column() {
-//        OutlinedTextField(
-//            value = selectedText,
-//            onValueChange = { selectedText = it },
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .onGloballyPositioned { coordinates ->
-//                    //This value is used to assign to the DropDown the same width
-//                    textfieldSize = coordinates.size.toSize()
-//                },
-//            label = { Text("Label") },
-//            trailingIcon = {
-//                Icon(icon, "contentDescription",
-//                     Modifier.clickable { expanded = !expanded })
-//            }
-//        )
-//        DropdownMenu(
-//            expanded = expanded,
-//            onDismissRequest = { expanded = false },
-//            modifier = Modifier
-//                .width(with(LocalDensity.current) { textfieldSize.width.toDp() })
-//        ) {
-//            suggestions.forEach { label ->
-//                DropdownMenuItem(onClick = {
-//                    selectedText = label
-//                }) {
-//                    Text(text = label)
-//                }
-//            }
-//        }
-//    }
-//}
+//endregion stackoverflow 2
 
 @Composable
 fun Header(
